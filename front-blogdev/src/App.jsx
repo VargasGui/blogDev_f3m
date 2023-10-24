ffc3da608ce286723c354ec70652ca587b678cdc
VargasGui <guilherme.vargas1h3@gmail.com>
2023-10-24 12:29:17 -0300

    Aula2410 - Estrutura Básica de Wireframe

--- front-blogdev/src/App.jsx
+++ front-blogdev/src/App.jsx
@@ -3,18 +3,22 @@ import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 
 import Home from './pages/Home/Home'
 import About from './pages/About/About'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 function App() {
 
   return (
     <>
       <BrowserRouter>
+      <Navbar />
         <div className='container'>
           <Routes>
             <Route path='/' element={<Home />} ></Route>
             <Route path='/about' element={<About />} ></Route>
           </Routes>
         </div>
+        <Footer />
       </BrowserRouter>
     </>
   )
